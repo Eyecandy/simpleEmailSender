79a88b516a6f228a366a465225a0d73fa9ca7b6e
Joakim Pedersen Niljford <joakimlunchactually@gmail.com>
2018-09-17 10:13:34 +0200

    simple form added

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 
 class WelcomeController extends AbstractController
@@ -14,5 +15,18 @@ class WelcomeController extends AbstractController
     public function index()
     {
         return $this->render("welcome/index.html.twig");
+    }
+
+    /**
+     * @Route("/hello/{name}", name="hello_page"),requirements={"name"="[A-Z,a-z]+"}
+     * @param string name
+     */
+    public function hello($name = "Joakim") {
+
+        return $this->render("hello_page.html.twig", 
+        [
+            "my_variable" => $name
+        ]
+    );
     }
 }
